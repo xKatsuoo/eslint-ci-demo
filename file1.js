4b77dbf855e21e0401b088b59bfadd1a71f2b8c7
xKatsuoo <jasonkrimmel@songoohan.de>
2023-11-20 11:46:41 +0100

    new#3

--- file1.js
+++ file1.js
@@ -1,4 +1,4 @@
-const add = (a,b)=>{
-    return a + b;
-};
-console.log(add(2, 3));
+function greet (name) {
+  console.log("Hello, " + name + "!");
+}
+greet("World");
--- file2.js
+++ file2.js
@@ -1,4 +1,4 @@
-const add=(a,b)=>{
-  return a+b;
-  }
-  console.log(add(2, 3))
+const add = (a,b)=>{
+  return a + b;
+};
+console.log(add(2, 3));
