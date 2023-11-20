63d2e1806083119572c42740a53ee500266b24ef
xKatsuoo <jasonkrimmel@songoohan.de>
2023-11-20 11:07:38 +0100

    Initial commit with linter setup

---
+++ file1.js
@@ -0,0 +1,10 @@
+function greet(name){ 
+    console.log("Hello, "+ name +"!"); 
+    } 
+    greet("World") 
+    file2.js : 
+    const add=(a,b)=>{ 
+    return a+b; 
+    } 
+    console.log(add(2, 3)) 
+    
